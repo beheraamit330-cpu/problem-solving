9d733fc6f98ee92a710c8ee9f210171d7841b556
beheraamit330-cpu <beheraamit330@gmail.com>
2026-08-27 22:59:38 +0530

    Time: 0 ms (100%), Space: 44.7 MB (36.98%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+
+        int sum2 = 0;
+
+        for (int i = 0; i < accounts.length; i++) {
+
+            int sum = 0;
+
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum += accounts[i][j];
+            }
+
+            if (sum2 < sum) {
+                sum2 = sum;
+            }
+        }
+
+        return sum2;
+    }
+}
